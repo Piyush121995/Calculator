d080a31a963ab52df024edeadb01124971fe5911
Piyush <piyush@Administrators-MacBook-Pro.local>
2024-09-26 14:06:41 +0530

    Added rest-Api

--- Project1/urls.py
+++ Project1/urls.py
@@ -1,18 +1,4 @@
-"""Project1 URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.conf.urls import url
 from calculator import views
@@ -21,6 +7,11 @@ urlpatterns = [
     url(r'^login/', views.login_page,name='login'),
     url(r'^cal/', views.calculate, name='calculator'),
     url(r'^signup/', views.sign_up,name='sign-up'),
-    url(r'^signup/success/', views.signup_success,name='signup_success')
+    url(r'^signup/success/', views.signup_success,name='signup_success'),
+
+# API-based view for mobile or external apps
+    url(r'^api/calculate/', views.api_calculate, name='api_calculate'),  # API endpoint for calculation
+    url(r'^api/users/', views.UserListCreateApiView.as_view(), name='api_user_list_create'),  # API endpoint for users
+    url(r'^api/users/(?P<username>[^/]+)/$', views.UserRetrieveUpdateDestroyApiView.as_view(), name='api_user_detail'), # API endpoint to retrieve, update, delete user by username
 
 ]
